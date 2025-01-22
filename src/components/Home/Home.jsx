cbf84b3ab1807ce850a55225179c9c6485d4f0a7
chirag.mandaviya <chirag.mandaviya12@gmail.com>
2025-01-22 23:27:26 +0530

    add header and home components and add react router

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,52 @@
+function Home() {
+    return (
+        <>
+            <div className='fixed top-0 left-0 h-screen w-full bg-[#081b29]'></div>
+            <h1 className="relative text-4xl font-bold text-white pt-10">
+                Welcome to the Main Page
+            </h1>
+            <h1 className="relative text-4xl font-bold text-white pt-10">
+                Welcome to the Main Page
+            </h1>
+            <h1 className="relative text-4xl font-bold text-white pt-10">
+                Welcome to the Main Page
+            </h1>
+            <h1 className="relative text-4xl font-bold text-white pt-10">
+                Welcome to the Main Page
+            </h1>
+            <h1 className="relative text-4xl font-bold text-white pt-10">
+                Welcome to the Main Page
+            </h1>
+            <h1 className="relative text-4xl font-bold text-white pt-10">
+                Welcome to the Main Page
+            </h1>
+            <h1 className="relative text-4xl font-bold text-white pt-10">
+                Welcome to the Main Page
+            </h1>
+            <h1 className="relative text-4xl font-bold text-white pt-10">
+                Welcome to the Main Page
+            </h1>
+            <h1 className="relative text-4xl font-bold text-white pt-10">
+                Welcome to the Main Page
+            </h1>
+            <h1 className="relative text-4xl font-bold text-white pt-10">
+                Welcome to the Main Page
+            </h1>
+            <h1 className="relative text-4xl font-bold text-white pt-10">
+                Welcome to the Main Page
+            </h1>
+            <h1 className="relative text-4xl font-bold text-white pt-10">
+                Welcome to the Main Page
+            </h1>
+            <h1 className="relative text-4xl font-bold text-white pt-10">
+                Welcome to the Main Page
+            </h1>
+            
+
+            
+
+        </>
+    )
+}
+
+export default Home
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './components/Home/Home.jsx'
+
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path='/' element={<Layout />}>
+            <Route path='/' element={<Home />} />
+        </Route>
+    )
+)
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>,
 )
